bdb0a030660039a388297768cf651af3b9fb64fc
hebo <hebo@tech-winning.com>
2022-06-30 19:03:43 +0800

    增加获取菜单接口

--- hebo-system/src/main/java/com/hebo/authDemo/controller/SysMenuController.java
+++ hebo-system/src/main/java/com/hebo/authDemo/controller/SysMenuController.java
@@ -1,9 +1,22 @@
 package com.hebo.authDemo.controller;
 
 
+import cn.hutool.core.collection.CollectionUtil;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.hebo.authDemo.entity.SysMenu;
+import com.hebo.authDemo.service.ISysMenuService;
+import com.hebo.dto.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Wrapper;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -14,7 +27,31 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-06-22
  */
 @RestController
-@RequestMapping("/sys-menu")
+@RequestMapping("/sysMenu")
 public class SysMenuController {
 
+    @Autowired
+    private ISysMenuService menuService;
+
+//    @PreAuthorize("hasAuthority('admin')")
+    @GetMapping("/listMenu")
+    public Response listMenu(){
+        List<SysMenu> sysMenus = menuService.list(Wrappers.<SysMenu>lambdaQuery().eq(SysMenu::getDeleteFlag, 0));
+        sysMenus.stream().forEach(x->{
+            setMenu(sysMenus,x);
+        });
+        return Response.success(sysMenus.stream().filter(x->0 == x.getParentId()).collect(Collectors.toList()));
+    }
+
+    private void setMenu(List<SysMenu> sysMenus,SysMenu sysMenu){
+        List<SysMenu> menus = new ArrayList<>();
+        if (CollectionUtil.isNotEmpty(sysMenus)){
+            sysMenus.forEach(x->{
+                if(x.getParentId().equals(sysMenu.getMenuId())){
+                    menus.add(x);
+                    sysMenu.setSysMenus(menus);
+                }
+            });
+        }
+    }
 }
--- hebo-system/src/main/java/com/hebo/authDemo/entity/SysMenu.java
+++ hebo-system/src/main/java/com/hebo/authDemo/entity/SysMenu.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
+import java.util.List;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -45,6 +46,9 @@ public class SysMenu implements GrantedAuthority {
 
     private Integer orderNum;
 
+    private Integer deleteFlag;
+    @TableField(exist = false)
+    List<SysMenu> sysMenus;
     @Override
     public String getAuthority() {
         return getMenuId().toString();
